02209c7d397a087d7f6783d07b3e50cc534fcf00
Marvin-Mawira <marvinmawira8@gmail.com>
2024-09-30 08:44:08 +0300

    library code

---
+++ library_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'library_app/book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'library_app/add_book.html', {'form': form})
+
+def edit_book(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library_app/edit_book.html', {'form': form})
+
+def delete_book(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'library_app/delete_book.html', {'book': book})
